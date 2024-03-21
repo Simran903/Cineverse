bfc2cfa0b31d9703a86aa0345784fcb76456d190
Simran <itssimran903@gmail.com>
2024-03-22 03:02:15 +0530

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,6 @@ const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
 }
-
 export const fetchDataFromAPI = async (url, params) => {
     try {
         const {data} = await axios.get(BASE_URL+ url, {
