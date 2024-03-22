c6ca87c9a3dfae803e4a1a1e935882bf6612fbff
Simran <itssimran903@gmail.com>
2024-03-22 22:26:01 +0530

    commit

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -6,11 +6,13 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
+
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
+
 import "./style.scss";
 
 const Carousel = ({ data, loading, endpoint, title }) => {
@@ -68,7 +70,8 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                                     className="carouselItem"
                                     onClick={() =>
                                         navigate(
-                                            `/${item.media_type || endpoint}/${item.id
+                                            `/${item.media_type || endpoint}/${
+                                                item.id
                                             }`
                                         )
                                     }
